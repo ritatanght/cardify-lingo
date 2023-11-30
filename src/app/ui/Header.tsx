06299f89d43b85c29c705e36bbe1388c0fad6725
ritatanght <doggymint@gmail.com>
2023-11-30 17:37:08 -0500

    Add tailwind css class

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -7,6 +7,7 @@ import {
   faUser,
   faBars,
   faChevronDown,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import "./Header.scss";
 
@@ -14,14 +15,14 @@ import { Category } from "../lib/definitions";
 import { Menu, Transition } from "@headlessui/react";
 const user = { id: 1, username: "john_doe" };
 
-function classNames(...classes:string[]) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Header({ categories }: { categories: Category[] }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <header>
+    <header className="px-4 md:px-6">
       {/* <ToastContainer
         position="bottom-center"
         autoClose={4000}
@@ -34,61 +35,59 @@ export default function Header({ categories }: { categories: Category[] }) {
         pauseOnHover
         theme="light"
       /> */}
-      <nav className="flex gap-x-2">
+      <nav className="flex gap-x-2 items-center">
         <Link href="/">
           <Image src="/logo.png" width={60} height={35.2} alt="Cardify logo" />
         </Link>
-        <div>
-          {/* dropdown */}
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-              <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                Categories
-                <FontAwesomeIcon
-                  icon={faChevronDown}
-                  className="-mr-1 h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </Menu.Button>
-            </div>
 
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="py-1">
-                  {categories.map((category) => (
-                    <Menu.Item key={category.name}>
-                      {({ active }) => (
-                        <Link
-                          href={`category/${category.id}`}
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-700",
-                            "block px-4 py-2 text-sm"
-                          )}
-                        >
-                          {category.name}
-                        </Link>
-                      )}
-                    </Menu.Item>
-                  ))}
-                </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </div>
+        {/* dropdown */}
+        <Menu as="div" className="dropdown py-3 relative inline-block">
+            <Menu.Button className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+              Categories
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className="-mr-1 h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+         
 
-        <div className="right justify-content-end">
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                {categories.map((category) => (
+                  <Menu.Item key={category.name}>
+                    {({ active }) => (
+                      <Link
+                        href={`category/${category.id}`}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        {category.name}
+                      </Link>
+                    )}
+                  </Menu.Item>
+                ))}
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+
+        <div className="ml-auto">
           <button
-            className="menu"
+            className="menu block md:hidden"
             aria-label="Open menu"
             onClick={() => setIsMenuOpen(true)}
           >
@@ -97,10 +96,12 @@ export default function Header({ categories }: { categories: Category[] }) {
           <div className={`nav-menu ${isMenuOpen ? " opened" : ""}`}>
             <button
               aria-label="Close menu"
+              className="block text-left md:hidden"
               onClick={() => setIsMenuOpen(false)}
             >
-              x
+              <FontAwesomeIcon icon={faXmark} />
             </button>
+
             {/* /<SearchBar closeMenu={() => setIsMenuOpen(false)} /> */}
 
             {user ? (
