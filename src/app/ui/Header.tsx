5128880cfb5b9c02f3d04b285eb0ec84f1fe1b17
ritatanght <doggymint@gmail.com>
2023-11-30 12:04:42 -0500

    Add dropdown to show categories

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -1,18 +1,22 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUser,
-  faBars
+  faBars,
+  faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
 import "./Header.scss";
-import { Category } from "../lib/definitions";
 
+import { Category } from "../lib/definitions";
+import { Menu, Transition } from "@headlessui/react";
 const user = { id: 1, username: "john_doe" };
 
-
+function classNames(...classes:string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Header({ categories }: { categories: Category[] }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -36,11 +40,51 @@ export default function Header({ categories }: { categories: Category[] }) {
         </Link>
         <div>
           {/* dropdown */}
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                Categories
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  className="-mr-1 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
 
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  {categories.map((category) => (
+                    <Menu.Item key={category.name}>
+                      {({ active }) => (
+                        <a
+                          href={`category/${category.id}`}
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          {category.name}
+                        </a>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </div>
-        {/* <NavDropdown title="Categories" id="nav-dropdown">
-          {dropDownItems}
-        </NavDropdown> */}
 
         <div className="right justify-content-end">
           <button
