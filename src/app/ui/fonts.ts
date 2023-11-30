90bbb323e8c77a818025b77d97fd840176dec4c7
ritatanght <doggymint@gmail.com>
2023-11-29 22:26:40 -0500

    Add fonts from next/font to optimize performance

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Playpen_Sans, Roboto } from "next/font/google";
+
+export const playpen = Playpen_Sans({ weight: ["400", "700"], subsets: ["latin"] });
+export const roboto = Roboto({ style: "normal", weight:["400","700"], subsets: ["latin"] });
