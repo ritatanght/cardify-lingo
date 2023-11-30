6894d1f716f25f2e6f2db3a9b9f3f5f699e6b99d
ritatanght <doggymint@gmail.com>
2023-11-30 12:01:31 -0500

    Add fetching of categories and pass to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,28 @@
 import type { Metadata } from 'next'
 import { roboto } from './ui/fonts'
 import './globals.scss'
-
+import Header from './ui/Header'
+import axios from "axios";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export const metadata: Metadata = {
   title: 'Cardify',
   description: 'Satisfy all your learning needs',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+    const { data } = await axios.get(
+      "http://localhost:8080/api/categories"
+    );
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased`}>{children}</body>
+      <body className={`${roboto.className} antialiased`}><Header categories={data}/>{children}</body>
     </html>
   );
 }
