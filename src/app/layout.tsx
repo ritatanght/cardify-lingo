a4d1f81ceeb40beb41389a7f8c3589af73285db3
ritatanght <doggymint@gmail.com>
2023-11-30 15:35:09 -0500

    Using request from api to get categories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,32 @@
-import type { Metadata } from 'next'
-import { roboto } from './ui/fonts'
-import './globals.scss'
-import Header from './ui/Header'
-import axios from "axios";
+import type { Metadata } from "next";
+import { roboto } from "./ui/fonts";
+import "./globals.scss";
+import Header from "./ui/Header";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
+import { getAllCategories } from "./lib/api";
 
 export const metadata: Metadata = {
-  title: 'Cardify',
-  description: 'Satisfy all your learning needs',
-}
+  title: "Cardify",
+  description: "Satisfy all your learning needs",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    const { data } = await axios.get(
-      "http://localhost:8080/api/categories"
-    );
+  const categories = await getAllCategories();
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased`}><Header categories={data}/>{children}</body>
+      <body
+        className={`${roboto.className} antialiased`}
+        suppressHydrationWarning={true}
+      >
+        <Header categories={categories} />
+        {children}
+      </body>
     </html>
   );
 }
