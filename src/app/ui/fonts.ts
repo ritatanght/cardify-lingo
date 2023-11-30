7403cc6ebc5f7b6639432c5856d4f65e0eeefbfa
ritatanght <doggymint@gmail.com>
2023-11-30 16:42:31 -0500

    Add properties to fix error

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,4 +1,9 @@
 import { Playpen_Sans, Roboto } from "next/font/google";
 
-export const playpen = Playpen_Sans({ weight: ["400", "700"], subsets: ["latin"] });
+export const playpen = Playpen_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  adjustFontFallback: false,
+});
 export const roboto = Roboto({ style: "normal", weight:["400","700"], subsets: ["latin"] });
