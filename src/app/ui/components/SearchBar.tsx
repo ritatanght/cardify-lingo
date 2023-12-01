21db8040c1e65c5a25cce994ed0a054f96c9e9e8
ritatanght <doggymint@gmail.com>
2023-11-30 20:18:35 -0500

    Add state, functions and render

--- src/app/ui/components/SearchBar.tsx
+++ src/app/ui/components/SearchBar.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useSearchParams, useRouter } from "next/navigation";
+//import { toast } from "react-toastify";
 
 export default function SearchBar({ closeMenu }: { closeMenu: () => void }) {
-  return <div className="SearchBar">SearchBar</div>;
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const [queryString, setQueryString] = useState("");
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    // if (!queryString) return toast.info("Please enter keyword for search");
+    if (!queryString) return console.log("Please enter keyword for search");
+    setQueryString("");
+    closeMenu();
+    const params = new URLSearchParams(searchParams);
+    params.set("query", queryString);
+    return replace(`/search?${params.toString()}`);
+  };
+
+  // pressing enter when focusing on the input field will submit the keyword for search
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handleSearch(e);
+    }
+  };
+
+  return (
+    <form className="SearchBar">
+      <input
+        name="query"
+        placeholder="Search"
+        aria-label="Search"
+        className="p-2 rounded-l-md"
+        value={queryString}
+        onChange={(e) => setQueryString(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button
+        className="rounded-l-none rounded-r-md px-3"
+        onClick={handleSearch}
+      >
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </button>
+    </form>
+  );
 }
