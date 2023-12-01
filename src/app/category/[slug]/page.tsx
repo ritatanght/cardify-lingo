914ad0eb637d6a5bb20b9290719ae7ecd3507512
ritatanght <doggymint@gmail.com>
2023-11-30 22:14:02 -0500

    Add back useSetList and loading state

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,10 +1,12 @@
-"use client";import { useState, useEffect } from "react";// import useSetsList from "../hooks/useSetsList";
-// import { useUser } from "../context/UserProvider";// import { toast } from "react-toastify";
+"use client";
+import { useState, useEffect } from "react";
+import useSetsList from "@/app/hooks/useSetsList";
 // import Spinner from "react-bootstrap/Spinner";
 import "./Category.scss";
 import { getCategoryById } from "@/app/lib/api";
 import { Category, Set } from "@/app/lib/definitions";
 import SetItem from "../../ui/components/SetItem";
+import Loading from "@/app/loading";
 
 type categoryData = {
   category: Category["name"];
@@ -12,16 +14,13 @@ type categoryData = {
 };
 
 const user = { id: 1, username: "john_doe" };
-const deleteSet = (setId: number) => {};
-
 
 export default function Page({ params }: { params: { slug: string } }) {
   // const { user } = useUser();
-  // const { sets, setSets, deleteSet } = useSetsList();
-  const [sets, setSets] = useState<Set[] | []>([]);
+  const { sets, setSets, deleteSet } = useSetsList();
   const [category, setCategory] = useState("");
-  //const [isLoading, setIsLoading] = useState(true);
-  console.log(category);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     getCategoryById(Number(params.slug))
       .then((data: categoryData) => {
@@ -30,24 +29,18 @@ export default function Page({ params }: { params: { slug: string } }) {
       })
       .catch((err) => {
         //toast.error(err);
-      });
-    //.finally(() => setIsLoading(false));
+      })
+      .finally(() => setIsLoading(false));
   }, [params.slug]);
 
-  // if (isLoading) {
-  //   return (
-  //     <Spinner animation="border" variant="primary" role="status">
-  //       <span className="visually-hidden">Loading...</span>
-  //     </Spinner>
-  //   );
-  // }
-
-  if (!category)
+  if (isLoading) {
     return (
-      <main>
-        <h1 className="text-center">Category Not Found</h1>
-      </main>
+      <Loading />
+      // <      <Spinner animation="border" variant="primary" role="status">
+      //         <span className="visually-hidden">Loading...</span>
+      //       </Spinner>>
     );
+  }
 
   // If a set is marked private, it will only show up in the search if the current user is the set owner
   const displaySet = sets.filter((set) => {
@@ -58,7 +51,6 @@ export default function Page({ params }: { params: { slug: string } }) {
     }
   });
 
- 
   const setsElements =
     Array.isArray(displaySet) &&
     displaySet.map((set) => (
@@ -72,9 +64,13 @@ export default function Page({ params }: { params: { slug: string } }) {
 
   return (
     <main className="Category-container">
-      <h1>
-        Category: <span>{category}</span>
-      </h1>
+      {category ? (
+        <h1>
+          Category: <span>{category}</span>
+        </h1>
+      ) : (
+        <h1 className="text-center">Category Not Found</h1>
+      )}
       {displaySet.length === 0 ? (
         <h2>There are currently no sets in this category.</h2>
       ) : (
