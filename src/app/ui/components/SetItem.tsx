26af526d4be5fb50a8feb36d7329074e4e7e61bc
ritatanght <doggymint@gmail.com>
2023-11-30 21:48:44 -0500

    Add state, functions and placeholder variables

--- src/app/ui/components/SetItem.tsx
+++ src/app/ui/components/SetItem.tsx
@@ -1,7 +1,62 @@
 import { Set } from "@/app/lib/definitions";
+import Link from "next/link";
+// import useFavButton from "../hooks/useFavButton";
+// import { useUser } from "../context/UserProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as fillHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as emptyHeart } from "@fortawesome/free-regular-svg-icons";
+import { faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import "../SetItem.scss";
 
-const SetItem = ({ set, setOwner }: { set: Set; setOwner: string }) => {
-  return <div>SetItem</div>;
+type setItemProps = { set: Set; setOwner: string; onDelete: () => void };
+
+const user = { id: 1, username: "john_doe" };
+const isLiked = true;
+const toggleLike = (set: Set) => {};
+
+const SetItem = ({ set, setOwner, onDelete }: setItemProps) => {
+  // const { user, favoriteSets } = useUser();
+  // const { isLiked, toggleLike, checkLiked } = useFavButton();
+
+  // useEffect(() => {
+  //   checkLiked(favoriteSets, set.id);
+  // }, [checkLiked, favoriteSets, set.id]);
+  return (
+    <div className="set-item-container">
+      <Link href={`/sets/${set.id}`}>
+        <p>{set.title}</p>
+      </Link>
+      <div className="set-item-right">
+        {user && user.id === set.user_id ? (
+          <div className="set-icons">
+            <button onClick={onDelete}>
+              <FontAwesomeIcon icon={faTrashCan} className="icon-primary" />
+            </button>
+            <Link href={`/sets/edit/${set.id}`}>
+              <FontAwesomeIcon icon={faPenToSquare} className="icon-primary" />
+            </Link>
+          </div>
+        ) : (
+          <span>{setOwner}</span>
+        )}
+        {user && (
+          <button onClick={() => toggleLike(set)}>
+            {isLiked ? (
+              <FontAwesomeIcon
+                icon={fillHeart}
+                className="icon-primary heart-icon"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={emptyHeart}
+                className="icon-primary heart-icon"
+              />
+            )}
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SetItem;
