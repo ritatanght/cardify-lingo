533cc7f5d5cd4f03b81bdd58c42e1026839398d5
ritatanght <doggymint@gmail.com>
2023-12-01 00:49:37 -0500

    Add state, functions, and placeholder userObj

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,74 @@
 "use client";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+
+import SetItem from "@/app/ui/components/SetItem";
+import useSetsList from "@/app/hooks/useSetsList";
+// import { useUser } from "../context/UserProvider";
+// import { toast } from "react-toastify";
+// import Spinner from "react-bootstrap/Spinner";
+import "./Search.scss";
+import { searchSets } from "@/app/lib/api";
+import Loading from "../loading";
+
+const user = { id: 1, username: "john_doe" };
+
 export default function Page() {
   const searchParams = useSearchParams();
-  const query = searchParams.get("query");
-  return <main>Search: {query}</main>;
+  const query: string | null = searchParams.get("query");
+  //  const { user } = useUser();
+
+  const { sets, setSets, deleteSet } = useSetsList();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    if (query) {
+      setIsLoading(true);
+      searchSets(query)
+        .then(setSets)
+        .catch((err) => {
+          // toast.error(err);
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }, [query]);
+
+  if (isLoading) {
+    return (
+      <main className="search-container">
+        <h1>
+          Search Results for &quot;<span>{query}</span>&quot;
+        </h1>
+        <Loading />
+      </main>
+    );
+  }
+
+  // If a set is marked private, it will only show up in the search if the current user is the set owner
+  const displaySet = sets.filter((set) => {
+    if (set.private) {
+      if (user && user.id === set.user_id) return set;
+    } else {
+      return set;
+    }
+  });
+
+  return (
+    <main className="search-container">
+      <h1>
+        Search Results for &quot;<span>{query}</span>&quot;
+      </h1>
+      {displaySet.length > 0 ? (
+        displaySet.map((set) => (
+          <SetItem
+            key={set.id}
+            set={set}
+            setOwner={set.username}
+            onDelete={() => deleteSet(set.id)}
+          />
+        ))
+      ) : (
+        <h1 className="no-results">Couldn&apos;t find anything!</h1>
+      )}
+    </main>
+  );
 }
