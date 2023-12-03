5726596e4ed52cc616fbf058b440bfe01b7e5814
ritatanght <doggymint@gmail.com>
2023-12-02 23:20:29 -0500

    Transfer styles to classes

--- src/app/ui/components/CardForm.tsx
+++ src/app/ui/components/CardForm.tsx
@@ -11,28 +11,36 @@ interface CardFormProps {
 const CardForm = ({ card, onUpdate, onDelete }: CardFormProps) => {
   return (
     <div className="card-container">
-      <div className="card-frontback-container">
-        <label className="card-container-front">
-          Front
+      <div className="flex w-full border-2 border-color-3 rounded-md mb-4 relative">
+        <div className="grow bg-color-1 rounded-md p-1">
+          <label className="block text-white font-bold text-center">
+            Front
+          </label>
           <input
             type="text"
-            placeholder="Front"
             name="front"
+            className="p-2 w-full"
             value={card.front}
             onChange={onUpdate}
           />
-        </label>
-        <label className="card-container-back">
-          Back
+        </div>
+        <div className="grow bg-color-4 rounded-md p-1">
+          <label className="block text-white font-bold text-center">
+            Back
+          </label>
           <input
             type="text"
-            placeholder="Back"
             name="back"
+            className="p-2 w-full"
             value={card.back}
             onChange={onUpdate}
           />
-          <FontAwesomeIcon icon={faTrash} onClick={onDelete} />
-        </label>
+        </div>
+        <FontAwesomeIcon
+          icon={faTrash}
+          onClick={onDelete}
+          className="absolute cursor-pointer top-2 right-2 hover:text-gray-600"
+        />
       </div>
     </div>
   );
