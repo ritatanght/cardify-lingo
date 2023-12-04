75aba28181e1e32143c6da6091e6c87fc2bb264d
ritatanght <doggymint@gmail.com>
2023-12-04 17:32:49 -0500

    Replace categories route with languages

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const cookieSession = require("cookie-session");
 
 const usersRouter = require("./api/users");
-const categoriesRouter = require("./api/categories");
+const languagesRouter = require("./api/languages");
 const setsRouter = require("./api/sets");
 const cardsRouter = require("./api/cards");
 const favoritesRouter = require("./api/favorites");
@@ -31,13 +31,13 @@ app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/sets", setsRouter);
 app.use("/api/cards", cardsRouter);
-app.use("/api/categories", categoriesRouter);
+app.use("/api/languages", languagesRouter);
 app.use("/api/favorites", favoritesRouter);
 app.use("/api/search", searchesRouter);
 
 // For any other route, serve React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 module.exports = app;
