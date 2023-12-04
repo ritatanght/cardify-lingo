d0603faf7429bcd50228d6f8b0b688e013dc9429
ritatanght <doggymint@gmail.com>
2023-12-04 14:43:46 -0500

    Add spinner icon using random color from list

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 export default function Loading() {
-  return <div className="text-center text-3xl my-8">Loading...</div>;
+  const randomColorNum = ["1", "2", "4"];
+  const color = `text-color-${
+    randomColorNum[Math.floor(Math.random() * randomColorNum.length)]
+  }`;
+  return (
+    <main className={`text-center text-5xl my-8 ${color}`}>
+      <FontAwesomeIcon icon={faSpinner} spinPulse />
+    </main>
+  );
 }
