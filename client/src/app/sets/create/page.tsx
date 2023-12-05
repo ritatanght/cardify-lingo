9c292d30eec76d4685f91c117fb956788b10aaa0
ritatanght <doggymint@gmail.com>
2023-12-04 20:25:08 -0500

    Update the request function in create and edit

--- client/src/app/sets/create/page.tsx
+++ client/src/app/sets/create/page.tsx
@@ -1,8 +1,7 @@
-import { getAllCategories } from "@/app/lib/api";
-import SetForm from "@/app/ui/components/SetForm";
+import { getAllLanguages } from "@/app/lib/api";import SetForm from "@/app/ui/components/SetForm";
 
 const Page = async () => {
-  const categories = await getAllCategories();
+  const categories = await getAllLanguages();
 
   return <SetForm mode="create" categories={categories} />;
 };
--- client/src/app/sets/edit/[id]/page.tsx
+++ client/src/app/sets/edit/[id]/page.tsx
@@ -1,11 +1,12 @@
-import { getAllCategories, getSet } from "@/app/lib/api";import { AxiosError, isAxiosError } from "axios";
+import { getAllLanguages, getSet } from "@/app/lib/api";
+import { AxiosError, isAxiosError } from "axios";
 import SetForm from "@/app/ui/components/SetForm";
 import { notFound } from "next/navigation";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   try {
     const [categories, setData] = await Promise.all([
-      getAllCategories(),
+      getAllLanguages(),
       getSet(params.id),
     ]);
 
