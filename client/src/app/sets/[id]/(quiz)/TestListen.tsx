62058dda2cde7330e8cb51c12ee986706c5e4dd3
ritatanght <doggymint@gmail.com>
2023-12-06 15:27:27 -0500

    Add state and functions to listen mode

--- client/src/app/sets/[id]/(quiz)/TestListen.tsx
+++ client/src/app/sets/[id]/(quiz)/TestListen.tsx
@@ -1,8 +1,44 @@
-import { Card } from "@/app/lib/definitions";
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 
-const TestListen = () => {
-  return <div>TestListen</div>;
+import React, { useState } from "react";
+import { Card } from "@/app/lib/definitions";
+interface TestListenProps {
+  card: Card;
+  endQuestion: (correct: boolean) => void;
+  speakText: () => void;
+}
+
+const TestListen = ({ card, endQuestion, speakText }: TestListenProps) => {
+  const [answer, setAnswer] = useState("");
+
+  const checkAnswer = () => {
+    if (answer === card.front) {
+      endQuestion(true);
+    } else {
+      endQuestion(false);
+    }
+    setAnswer("")
+  };
+  return (
+    <>
+      <p>What does this mean in your own language?</p>
+      <button
+        className="text-2xl text-color-1 transition-colors hover:text-gray-500"
+        onClick={speakText}
+      >
+        <FontAwesomeIcon icon={faVolumeHigh} />
+      </button>
+      <input
+        className="block mx-auto my-4 p-2 rounded"
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
+      />
+      <button className="btn" onClick={checkAnswer}>
+        Submit Answer
+      </button>
+    </>
+  );
 };
 
 export default TestListen;
