788d30c302412b237112daf2471971841b84ab5e
ritatanght <doggymint@gmail.com>
2023-12-06 15:26:08 -0500

    Add state and functions, randomly ask the translation for front or back

--- client/src/app/sets/[id]/(quiz)/TestRead.tsx
+++ client/src/app/sets/[id]/(quiz)/TestRead.tsx
@@ -1,8 +1,39 @@
 import { Card } from "@/app/lib/definitions";
-import React from "react";
+import { useState } from "react";
+interface TestRead {
+  card: Card;
+  endQuestion: (correct: boolean) => void;
+}
+const TestRead = ({ card, endQuestion }: TestRead) => {
+  const [answer, setAnswer] = useState("");
+  const [questionSide, setQuestionSide] = useState<keyof Card>(
+    Math.random() < 0.5 ? "front" : "back"
+  );
 
-const TestRead = ({ text }: { text: string }) => {
-  return <p className="text-2xl">{text}</p>;
+  const checkAnswer = () => {
+    if (questionSide === "front") {
+      endQuestion(answer === card.back);
+    } else {
+      endQuestion(answer === card.front);
+    }
+    setAnswer("");
+  };
+
+  return (
+    <>
+      <p className="text-2xl">
+        What is the translation of {card[questionSide]}
+      </p>
+      <input
+        className="block mx-auto my-4 p-2 rounded"
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
+      />
+      <button className="btn" onClick={checkAnswer}>
+        Submit Answer
+      </button>
+    </>
+  );
 };
 
 export default TestRead;
