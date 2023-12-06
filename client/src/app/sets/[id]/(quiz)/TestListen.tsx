bf54f7bb8a008c79975762e6bfa88d946c3c37fc
ritatanght <doggymint@gmail.com>
2023-12-06 14:03:15 -0500

    Add two component to be rendered randomly for quiz

---
+++ client/src/app/sets/[id]/(quiz)/TestListen.tsx
@@ -0,0 +1,8 @@
+import { Card } from "@/app/lib/definitions";
+import React from "react";
+
+const TestListen = () => {
+  return <div>TestListen</div>;
+};
+
+export default TestListen;
---
+++ client/src/app/sets/[id]/(quiz)/TestRead.tsx
@@ -0,0 +1,8 @@
+import { Card } from "@/app/lib/definitions";
+import React from "react";
+
+const TestRead = ({ text }: { text: string }) => {
+  return <p className="text-2xl">{text}</p>;
+};
+
+export default TestRead;
