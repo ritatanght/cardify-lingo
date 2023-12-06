6f6eb9570142ff9f6040f3f4196da7d71cf703a2
ritatanght <doggymint@gmail.com>
2023-12-06 15:23:01 -0500

    Randomly generate test mode and pass utterance to listen mode

--- client/src/app/sets/[id]/(quiz)/QuizSet.tsx
+++ client/src/app/sets/[id]/(quiz)/QuizSet.tsx
@@ -3,45 +3,71 @@ import { Card } from "@/app/lib/definitions";
 import { randomSort } from "@/app/lib/utils";
 import TestRead from "./TestRead";
 import TestListen from "./TestListen";
+import { toast } from "react-toastify";
 interface QuizSetProps {
   cards: Card[];
+  voice: SpeechSynthesisVoice;
 }
+const testModeArr = ["read", "listen"];
 
-const QuizSet = ({ cards }: QuizSetProps) => {
+const QuizSet = ({ cards, voice }: QuizSetProps) => {
   const [question, setQuestion] = useState(0);
-  const [answer, setAnswer] = useState("");
+
   const [shuffledCards, setShuffledCards] = useState(randomSort(cards));
   const [score, setScore] = useState(0);
+  const [testMode, setTestMode] = useState(
+    Math.random() < 0.5 ? testModeArr[0] : testModeArr[1]
+  );
 
-  const checkAnswer = () => {
-    if (answer === shuffledCards[question].back) {
+  const generateTestMode = () =>
+    setTestMode(Math.random() < 0.5 ? testModeArr[0] : testModeArr[1]);
+
+  const endQuestion = (correct: boolean) => {
+    // show whether the input is correct
+    if (correct) {
       console.log("Correct");
       setScore((prev) => prev + 1);
     } else {
       console.log("Incorrect.");
     }
+    // move to the next question
     if (question < cards.length - 1) {
+      generateTestMode();
       setQuestion((prev) => prev + 1);
     } else {
       console.log(`Quiz Finished!`);
     }
   };
+
+  const speakText = () => {
+    const synth = window.speechSynthesis;
+    if (!synth)
+      return toast.error("Your browser does not support Speech Synthesis");
+    const utterance = new SpeechSynthesisUtterance(
+      shuffledCards[question].back
+    );
+    utterance.voice = voice;
+
+    // cancel the ongoing utterance if there is any
+    if (synth.speaking) {
+      synth.cancel();
+    }
+    synth.speak(utterance);
+  };
+
   return (
     <div className="mt-2 text-center md:mt-6 min-h-[200px]">
       <h3 className="text-2xl">Q{question + 1}:</h3>
-      {Math.random() < 0.5 ? (
-        <TestRead text={shuffledCards[question].front} />
-      ) : (
-        <TestListen />
+      {testMode === "read" && (
+        <TestRead card={shuffledCards[question]} endQuestion={endQuestion} />
       )}
-      <input
-        className="block mx-auto my-4 p-2 rounded"
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-      />
-      <button className="btn" onClick={checkAnswer}>
-        Submit Answer
-      </button>
+      {testMode === "listen" && (
+        <TestListen
+          card={shuffledCards[question]}
+          speakText={speakText}
+          endQuestion={endQuestion}
+        />
+      )}
     </div>
   );
 };
