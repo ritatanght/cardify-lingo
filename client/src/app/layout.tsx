b66733e6e2a0137d752eae8fbaa9b44e72c1c87e
ritatanght <doggymint@gmail.com>
2023-12-06 22:29:39 -0500

    Add favicon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,4 @@
-import type { Metadata } from "next";
-import { roboto } from "./ui/fonts";import Providers from "./providers";import Header from "./ui/components/Header";
+import type { Metadata } from "next";import { roboto } from "./ui/fonts";import Providers from "./providers";import Header from "./ui/components/Header";
 import { getAllLanguages } from "./lib/api";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -9,6 +8,9 @@ config.autoAddCss = false;
 export const metadata: Metadata = {
   title: "Cardify",
   description: "Satisfy all your learning needs",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default async function RootLayout({
