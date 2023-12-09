11b50d950fa4691811762ceaf370dc7947005bfb
ritatanght <doggymint@gmail.com>
2023-12-08 20:54:03 -0500

    Move register link to login form

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -15,12 +15,6 @@ const Page = async () => {
     <main>
       <h1 className={`text-3xl mb-4 ${playpen.className}`}>Login</h1>
       <LoginForm />
-      <p className="mt-4">
-        Don&apos;t have an account?{" "}
-        <Link className="text-darken-5-200 hover:underline" href="/register">
-          Register
-        </Link>
-      </p>
     </main>
   );
 };
