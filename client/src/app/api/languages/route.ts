a752973d89bdbade8caba85cb3adc8d228bf9789
ritatanght <doggymint@gmail.com>
2023-12-08 21:56:47 -0500

    Add route for getting all languages

---
+++ client/src/app/api/languages/route.ts
@@ -0,0 +1,16 @@
+const languages = require("@/../db/queries/languages");
+
+export async function GET(request: Request) {
+  try {
+    const data = await languages.getAllLanguages();
+    return Response.json(data);
+  } catch (err) {
+    console.error(err);
+    return Response.json(
+      { message: "Error fetching languages" },
+      {
+        status: 500,
+      }
+    );
+  }
+}
