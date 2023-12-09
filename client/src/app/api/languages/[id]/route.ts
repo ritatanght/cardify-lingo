94ba080248d6b57f2cc341961b7f8c25799830ad
ritatanght <doggymint@gmail.com>
2023-12-08 21:57:19 -0500

    Add route for getting specifc language

---
+++ client/src/app/api/languages/[id]/route.ts
@@ -0,0 +1,18 @@
+const languages = require("@/../db/queries/languages");
+const sets = require("@/../db/queries/sets");
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+  const languagePromise = languages.getLanguageById(id);
+  const setsPromise = sets.getSetsByLanguageId(id);
+  try {
+    const [language, sets] = await Promise.all([languagePromise, setsPromise]);
+    if (!language)
+      return Response.json({ message: "Language not found" }, { status: 404 });
+    return Response.json({ language: language.name, sets });
+  } catch (err) {
+    console.error(err);
+  }
+}
