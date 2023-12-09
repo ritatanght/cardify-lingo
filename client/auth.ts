2190c9f3cef0f293b81967e1e8f9c20cce675f62
ritatanght <doggymint@gmail.com>
2023-12-08 19:17:42 -0500

    Add auth.ts and add providers

---
+++ client/auth.ts
@@ -0,0 +1,59 @@
+import { logInUser } from "@/app/lib/api";
+import type {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import type { NextAuthOptions } from "next-auth";
+import { getServerSession } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+// Pass this to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: "1",
+          name: "johnDoe",
+          password: "johnss",
+          email: "john.doe@example.com",
+        };
+        if (
+          credentials?.email === user.email &&
+          credentials.password === user.password
+        ) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+
+      },
+    }),
+  ],
+} satisfies NextAuthOptions;
+
+// Use it in server contexts
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, authOptions);
+}
