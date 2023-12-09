597f96a7a55f33ea620494cf96bd5ce7af7586c4
ritatanght <doggymint@gmail.com>
2023-12-09 11:47:20 -0500

    Add endpoint for getting user info from session

--- client/src/app/api/users/route.ts
+++ client/src/app/api/users/route.ts
@@ -1,6 +1,20 @@
 const users = require("@/../db/queries/users");
 import { DatabaseError } from "pg";
+import { auth } from "../../../../auth";
 import bcrypt from "bcrypt";
+
+export async function GET(req: any, res: any) {
+  const session = await auth();
+
+  try {
+    const user = await users.getUserInfoByEmail(session?.user?.email);
+ 
+    return Response.json(user, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return Response.json({ message: "Error fetching user" }, { status: 500 });
+  }
+}
 
 // register user
 export async function POST(request: Request) {
