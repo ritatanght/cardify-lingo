9c8117c8d85930818a1a63aa3edcbff7edf33654
ritatanght <doggymint@gmail.com>
2023-12-08 21:36:58 -0500

    Fix typescript error for PORT

--- client/db/db.config.ts
+++ client/db/db.config.ts
@@ -1,5 +1,5 @@
 // Database connections
-const { Pool } = require("pg");
+import { Pool } from "pg";
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: DB_USER,
   host: DB_HOST,
   password: DB_PASSWORD,
-  port: DB_PORT,
+  port: Number(DB_PORT),
   database: DB_DATABASE,
   //connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
@@ -19,4 +19,4 @@ pool
   })
   .catch(console.error);
 
-module.exports = pool;
+export default pool;
