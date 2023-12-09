ad02381a7d1d66e6d4f335aa27cf924253bbf856
ritatanght <doggymint@gmail.com>
2023-12-09 15:38:58 -0500

    Add endpoints for liking and unliking sets

--- client/src/app/api/favorites/[id]/route.ts
+++ client/src/app/api/favorites/[id]/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { auth } from "../../../../../auth";
 
 const favorites = require("@/../db/queries/favorites");
@@ -6,12 +7,45 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const session = await auth();
   const userId = params.id;
   try {
     const favoriteSets = await favorites.getFavoritesByUserId(userId);
     return Response.json(favoriteSets);
   } catch (err) {
     return Response.json({ message: err }, { status: 500 });
+  }
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const setId = params.id;
+  const { userId } = await request.json();
+  console.log(userId);
+  try {
+    await favorites.addFavoriteByUserAndSet(userId, setId);
+    return Response.json("success", { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return Response.json({ message: "Error liking the set" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const setId = params.id;
+  const { userId } = await request.json();
+  try {
+    await favorites.removeFavoriteByUserAndSet(userId, setId);
+    return Response.json("success", { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return Response.json(
+      { message: "Error unliking the set" },
+      { status: 500 }
+    );
   }
 }
