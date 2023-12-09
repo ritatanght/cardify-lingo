fe657ede8db36b6bf9d7d8cd02fc6ae78f69fa6d
ritatanght <doggymint@gmail.com>
2023-12-08 21:56:29 -0500

    Add type for parameter

--- client/db/queries/languages.ts
+++ client/db/queries/languages.ts
@@ -10,7 +10,7 @@ const getAllLanguages = () => {
     .then((data) => data.rows);
 };
 
-const getLanguageById = (languageId) => {
+const getLanguageById = (languageId:string) => {
   return db
     .query(
       `
