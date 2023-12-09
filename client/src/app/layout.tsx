566b601da4f7e08b54291637c3044ea395314c90
ritatanght <doggymint@gmail.com>
2023-12-08 19:19:27 -0500

    Get user from session and pass to header

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,12 @@
-import type { Metadata } from "next";import { roboto } from "./ui/fonts";import Providers from "./providers";import Header from "./ui/components/Header";
+import type { Metadata } from "next";import { roboto } from "./ui/fonts";
+import Providers from "./providers";
+import Header from "./ui/components/Header";
 import { getAllLanguages } from "./lib/api";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./globals.scss";
+import { auth } from "../../auth";
+
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
@@ -19,6 +23,9 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const languages = await getAllLanguages();
+  const session = await auth();
+  const user = session?.user?.name;
+
   return (
     <html lang="en">
       <body
@@ -26,7 +33,7 @@ export default async function RootLayout({
         suppressHydrationWarning={true}
       >
         <Providers>
-          <Header languages={languages} />
+          <Header languages={languages} user={user} />
           <div className="content-container md:px-6 md:py-8">{children}</div>
         </Providers>
       </body>
