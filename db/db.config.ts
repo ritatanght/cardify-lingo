dc26eff07c9c971aa755ad3897a3a3fb5850fb23
ritatanght <doggymint@gmail.com>
2023-12-11 22:44:39 -0500

    Update to vercel postgres connection

--- db/db.config.ts
+++ db/db.config.ts
@@ -1,15 +1,8 @@
 // Database connections
 import { Pool } from "pg";
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
-
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  password: DB_PASSWORD,
-  port: Number(DB_PORT),
-  database: DB_DATABASE,
-  //connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 pool
