7d1089f6f609a9db4e613a64c7e11c845b468ea0
ritatanght <doggymint@gmail.com>
2023-12-18 16:47:32 -0500

    Revise to return only the url

--- src/app/api/image/upload/route.ts
+++ src/app/api/image/upload/route.ts
@@ -7,11 +7,11 @@ export async function POST(request: Request): Promise<NextResponse> {
 
   // ⚠️ The below code is for App Router Route Handlers only
   if (filename && request.body) {
-    const blob = await put(filename, request.body, {
+    const { url } = await put(filename, request.body, {
       access: "public",
     });
 
-    return NextResponse.json(blob);
+    return NextResponse.json({url});
   }
   return NextResponse.json({ message: "No file attached", status: 400 });
 }
