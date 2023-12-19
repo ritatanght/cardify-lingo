c4dbff2fc9362d63fc2f2f1dae91adc5aafe5bc5
ritatanght <doggymint@gmail.com>
2023-12-18 22:09:39 -0500

    Update EditCardModal component to handle image uploads and deletions

--- src/app/sets/[id]/_components/(view)/EditCardModal.tsx
+++ src/app/sets/[id]/_components/(view)/EditCardModal.tsx
@@ -1,9 +1,11 @@
 import { Card } from "@/types/definitions";
-import { useState, Fragment } from "react";
+import { useState, Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { toast } from "react-toastify";
-import { editCardById } from "@/lib/services";
-import { useRouter } from "next/navigation";
+import { deleteImage, editCardById, uploadImage } from "@/lib/services";
+import { IoIosRemoveCircle } from "react-icons/io";
+import { FaImage } from "react-icons/fa6";
+import Image from "next/image";
 
 interface modalProps {
   show: boolean;
@@ -12,35 +14,62 @@ interface modalProps {
   onUpdate: (card: Card) => void;
 }
 const EditCardModal = ({ show, onHide, card, onUpdate }: modalProps) => {
-  const router = useRouter();
+  const inputFile = useRef<HTMLInputElement | null>(null);
   const [front, setFront] = useState(card.front);
   const [back, setBack] = useState(card.back);
+  const [image, setImage] = useState<File | string | null>(card.image_url);
 
-  const handleSubmit = () => {
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const allowedFileTypes = [
+      "image/jpg",
+      "image/jpeg",
+      "image/png",
+      "image/gif",
+    ];
+    const upload = e.target.files?.length && e.target.files[0];
+    if (upload && allowedFileTypes.includes(upload.type)) {
+      setImage(upload);
+    } else {
+      toast.error("Invalid File Selected");
+    }
+  };
+
+  const handleSubmit = async () => {
     if (!front || !back)
       return toast.info("Front and back text cannot be empty");
-    // Update the card data on the backend first
-    editCardById(card.id, { front, back })
-      .then((response) => {
-        if (response.data.success) {
-          // If successful, update the UI
-          onUpdate({ ...card, front, back });
-          toast.success("Changes have been saved.", {
-            position: "top-center",
-          });
-          onHide();
-        } else {
-          console.error("Error updating card: ", response.data.message);
-        }
-      })
-      .catch((err) => {
-        if (err.response.status === 401) {
-          toast.info(err.response.data.message);
-          return router.replace("/login");
-        } else {
-          toast.error(err);
-        }
+    // Added a new image
+    let imageUrl = null;
+    if (image && typeof image !== "string") {
+      // upload the new image and get the url
+      imageUrl = await uploadImage(image);
+    }
+    // there was a image and no deleted or a new image uploaded
+    if (card.image_url && (!image || (image && typeof image !== "string"))) {
+      // clean up the previous image
+      await deleteImage(card.image_url);
+    }
+
+    // Update the card data on the backend
+    try {
+      const response = await editCardById(card.id, {
+        front,
+        back,
+        image_url: imageUrl,
       });
+
+      if (response.data.success) {
+        // If successful, update the UI
+        onUpdate({ ...card, front, back, image_url: imageUrl });
+        toast.success("Changes have been saved.", {
+          position: "top-center",
+        });
+        onHide();
+      } else {
+        console.error("Error updating card: ", response.data.message);
+      }
+    } catch (err) {
+      toast.error("Error updating card: " + err);
+    }
   };
 
   return (
@@ -76,6 +105,47 @@ const EditCardModal = ({ show, onHide, card, onUpdate }: modalProps) => {
                 >
                   Edit Card
                 </Dialog.Title>
+                <input
+                  type="file"
+                  accept="image/*"
+                  name="image"
+                  onChange={handleImageChange}
+                  className="hidden"
+                  ref={inputFile}
+                />
+                {image ? (
+                  <div className="border-2 border-color-3 aspect-square w-1/2 rounded relative mx-auto mb-4">
+                    <button className="absolute text-2xl bottom-1 right-1 bg-white rounded-full text-color-1 hover:text-color-heart z-10">
+                      <IoIosRemoveCircle
+                        aria-label="Remove image"
+                        onClick={() => setImage(null)}
+                      />
+                    </button>
+                    <Image
+                      src={
+                        typeof image === "string"
+                          ? image
+                          : URL.createObjectURL(image)
+                      }
+                      alt=""
+                      unoptimized={false}
+                      // height="100"
+                      // width="100"
+                      fill={true}
+                      sizes="200px"
+                    />
+                  </div>
+                ) : (
+                  <button
+                    className="border-dashed border-2 w-1/2 aspect-square rounded flex flex-col justify-center items-center transition mx-auto mb-4 text-slate-300 hover:text-gray-600"
+                    onClick={() =>
+                      inputFile.current && inputFile.current.click()
+                    }
+                  >
+                    <FaImage className="text-2xl" aria-label="Add image" />
+                    <span className="text-xs">Image</span>
+                  </button>
+                )}
                 <form className="pb-6 text-center">
                   <div className="card-container rounded-md p-1 bg-color-1 mb-4">
                     <label
@@ -111,7 +181,7 @@ const EditCardModal = ({ show, onHide, card, onUpdate }: modalProps) => {
                 <div className="sm:flex sm:flex-row-reverse">
                   <button
                     type="button"
-                    className="btn w-full rounded-md bg-color-5 px-3 py-2 text-sm font-bold shadow-sm sm:ml-3 sm:w-auto"
+                    className="btn w-full rounded-md bg-color-5 px-3 py-2 text-sm font-bold shadow-sm sm:ml-3 sm:w-auto mb-2 md:mb-0"
                     onClick={handleSubmit}
                   >
                     Save Changes
