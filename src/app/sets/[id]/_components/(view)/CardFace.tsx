207aae794c0376836e75dc5160f76a5d4963745b
ritatanght <doggymint@gmail.com>
2023-12-18 22:10:37 -0500

    Update type for props

--- src/app/sets/[id]/_components/(view)/CardFace.tsx
+++ src/app/sets/[id]/_components/(view)/CardFace.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 interface CardFaceProps {
   position: string;
   text: string;
-  imageUrl?: string;
+  imageUrl?: string | null;
   voice: SpeechSynthesisVoice | null;
   isSetOwner: boolean;
   onEdit: () => void;
