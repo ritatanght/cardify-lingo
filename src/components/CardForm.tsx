9f289718b22eb4ab09119321f64c880544a200aa
ritatanght <doggymint@gmail.com>
2023-12-25 22:47:51 -0500

    Turn CardForm into draggable with dnd-kit

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -3,16 +3,14 @@ import { FaRegTrashCan, FaImage } from "react-icons/fa6";
 import { useRef } from "react";
 import { IoIosRemoveCircle } from "react-icons/io";
 import { RxDragHandleHorizontal } from "react-icons/rx";
+import { useDraggable, useDroppable } from "@dnd-kit/core";
 import Image from "next/image";
+
 interface CardFormProps {
   card: CardFormData;
   onUpdate: (e: React.BaseSyntheticEvent) => void;
   onDelete: () => void;
   selectedLanguage: string;
-  onDragStart: () => void;
-  onDragEnd: () => void;
-  onDragOver: (e: React.DragEvent<HTMLDivElement>) => void;
-  isDragging: Boolean;
 }
 
 const CardForm = ({
@@ -20,12 +18,16 @@ const CardForm = ({
   onUpdate,
   onDelete,
   selectedLanguage,
-  onDragStart,
-  onDragEnd,
-  onDragOver,
-  isDragging: dragging,
 }: CardFormProps) => {
   const inputFile = useRef<HTMLInputElement | null>(null);
+  const { attributes, listeners, setNodeRef, transform, isDragging } =
+    useDraggable({
+      id: card.id || "cardId",
+    });
+
+  const { setNodeRef: dropNodeRef } = useDroppable({
+    id: card.id || "cardId",
+  });
 
   // when clicking the remove icon on the uploaded image
   const handleRemoveImage = (
@@ -53,95 +55,103 @@ const CardForm = ({
   };
 
   return (
-    <div
-      className={`card-container rounded-md border-2 border-color-3 mb-4 ${
-        dragging ? "opacity-50" : ""
-      }`}
-      draggable
-      onDragStart={onDragStart}
-      onDragEnd={onDragEnd}
-      onDragOver={onDragOver}
-    >
-      <div className="w-full md:static bg-gray-50 text-right">
-        <button
-          className="inline p-1 transition text-lg hover:text-gray-600"
-          onClick={(e) => e.preventDefault()}
-          aria-label="Drag and drop card"
-        >
-          <RxDragHandleHorizontal />
-        </button>
-        <button className="inline p-1 transition text-lg hover:text-gray-600">
-          <FaRegTrashCan
+    <div ref={dropNodeRef}>
+      <div
+        className={`card-container rounded-md bg-white border-2 border-color-3 mb-4 touch-manipulation cursor-auto ${
+          isDragging ? "opacity-50" : ""
+        } `}
+        ref={setNodeRef}
+        {...listeners}
+        {...attributes}
+      >
+        <div className="w-full md:static bg-gray-50 text-right">
+          <button
+            className={`inline p-1 transition text-lg  ${
+              isDragging ? " cursor-grabbing" : " cursor-grab"
+            }`}
+            onClick={(e) => e.preventDefault()}
+            aria-label="Drag and drop card"
+          >
+            <RxDragHandleHorizontal />
+          </button>
+          <button
+            className="inline p-1 transition text-lg hover:text-gray-600"
             onClick={handleDeleteBtnClick}
-            aria-label="Remove Card"
-          />
-        </button>
-      </div>
-      <div className="flex w-full relative flex-wrap justify-end items-center gap-1 md:gap-0 md:items-stretch">
-        <div className="grow bg-color-1 rounded-md p-1">
-          <label className="block text-white font-bold text-center">
-            Front
-          </label>
-          <input
-            type="text"
-            name="front"
-            className="p-2 w-full"
-            value={card.front}
-            onChange={onUpdate}
-            placeholder="English"
-          />
+            data-no-dnd="true"
+          >
+            <FaRegTrashCan aria-label="Remove Card" />
+          </button>
         </div>
-        <div className="grow bg-color-4 rounded-md p-1">
-          <label className="block text-white font-bold text-center">Back</label>
-          <input
-            type="text"
-            name="back"
-            className="p-2 w-full"
-            value={card.back}
-            onChange={onUpdate}
-            placeholder={selectedLanguage}
-          />
-        </div>
-        <div className="md:w-24 w-20 p-1 aspect-square md:aspect-auto rounded text-slate-300 hover:text-gray-600">
-          <input
-            type="file"
-            accept="image/*"
-            name="image"
-            onChange={onUpdate}
-            className="hidden"
-            ref={inputFile}
-          />
-          {card.image_url || card.image ? (
-            <div className="border-2 h-full rounded flex justify-center items-center relative overflow-clip">
-              <button>
-                <IoIosRemoveCircle
-                  className="absolute bottom-0.5 right-0.5 md:right-1 bg-white rounded-full text-color-1 hover:text-color-heart z-10"
-                  aria-label="Remove image"
-                  onClick={handleRemoveImage}
+        <div
+          className="flex w-full relative flex-wrap justify-end items-center gap-1 md:gap-0 md:items-stretch"
+          data-no-dnd="true"
+        >
+          <div className="grow bg-color-1 rounded-md p-1">
+            <label className="block text-white font-bold text-center">
+              Front
+            </label>
+            <input
+              type="text"
+              name="front"
+              className="p-2 w-full"
+              value={card.front}
+              onChange={onUpdate}
+              placeholder="English"
+            />
+          </div>
+          <div className="grow bg-color-4 rounded-md p-1">
+            <label className="block text-white font-bold text-center">
+              Back
+            </label>
+            <input
+              type="text"
+              name="back"
+              className="p-2 w-full"
+              value={card.back}
+              onChange={onUpdate}
+              placeholder={selectedLanguage}
+            />
+          </div>
+          <div className="md:w-24 w-20 p-1 aspect-square md:aspect-auto rounded text-slate-300 hover:text-gray-600">
+            <input
+              type="file"
+              accept="image/*"
+              name="image"
+              onChange={onUpdate}
+              className="hidden"
+              ref={inputFile}
+            />
+            {card.image_url || card.image ? (
+              <div className="border-2 h-full rounded flex justify-center items-center relative overflow-clip">
+                <button onClick={handleRemoveImage}>
+                  <IoIosRemoveCircle
+                    className="absolute bottom-0.5 right-0.5 md:right-1 bg-white rounded-full text-color-1 hover:text-color-heart z-10"
+                    aria-label="Remove image"
+                  />
+                </button>
+                <Image
+                  src={
+                    card.image
+                      ? URL.createObjectURL(card.image)
+                      : card.image_url || ""
+                  }
+                  unoptimized={false}
+                  height="100"
+                  width="100"
+                  alt="Image on card"
+                  className="md:max-h-[60.5px] w-auto h-full w-auto"
                 />
+              </div>
+            ) : (
+              <button
+                className="border-dashed border-2 w-full h-full rounded flex flex-col justify-center items-center transition"
+                onClick={handleAddImageBtnClick}
+              >
+                <FaImage className="text-2xl" aria-label="Add image" />
+                <span className="text-xs">Image</span>
               </button>
-              <Image
-                src={
-                  card.image
-                    ? URL.createObjectURL(card.image)
-                    : card.image_url || ""
-                }
-                unoptimized={false}
-                height="100"
-                width="100"
-                alt="Image on card"
-                className="md:max-h-[60.5px] w-auto h-full w-auto"
-              />
-            </div>
-          ) : (
-            <button
-              className="border-dashed border-2 w-full h-full rounded flex flex-col justify-center items-center transition"
-              onClick={handleAddImageBtnClick}
-            >
-              <FaImage className="text-2xl" aria-label="Add image" />
-              <span className="text-xs">Image</span>
-            </button>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
