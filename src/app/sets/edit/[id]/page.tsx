cfd4bb40efe05c19677aa274d094c42227a41a5d
ritatanght <doggymint@gmail.com>
2023-12-25 21:25:38 -0500

    Move the display for non set owner trying to edit set to page.tsx

--- src/app/sets/edit/[id]/page.tsx
+++ src/app/sets/edit/[id]/page.tsx
@@ -3,12 +3,14 @@ import { getSetInfoById } from "@/db/queries/sets";
 import { getCardsBySetId } from "@/db/queries/cards";
 import SetForm from "@/components/SetForm";
 import { notFound } from "next/navigation";
+import { auth } from "@/../auth";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const setId = params.id;
   const languagesPromise = getAllLanguages();
   const setPromise = getSetInfoById(setId);
   const cardsPromise = getCardsBySetId(setId);
+  const session = await auth();
 
   try {
     const [languages, setData, cardsData] = await Promise.all([
@@ -16,6 +18,17 @@ const Page = async ({ params }: { params: { id: string } }) => {
       setPromise,
       cardsPromise,
     ]);
+
+    // display when the user is not the set's owner
+    if (!session || session.user.id !== setData?.set.user_id) {
+      return (
+        <main>
+          <h1 className="text-xl text-center">
+            Sorry, you don&apos;t have permission to edit this set!
+          </h1>
+        </main>
+      );
+    }
 
     return (
       <SetForm
