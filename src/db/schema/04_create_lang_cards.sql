16108af33b6236f965e5aef24bf8ca8c710923fc
ritatanght <doggymint@gmail.com>
2023-12-25 21:26:26 -0500

    Add sequence column to lang_cards table

--- src/db/schema/04_create_lang_cards.sql
+++ src/db/schema/04_create_lang_cards.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS lang_cards CASCADE;
 CREATE TABLE lang_cards (
   id SERIAL PRIMARY KEY,
   lang_set_id INT REFERENCES lang_sets(id),
+  sequence INT NOT NULL,
   front VARCHAR(255) NOT NULL,
   back VARCHAR(255) NOT NULL,
   image_url VARCHAR(255),
