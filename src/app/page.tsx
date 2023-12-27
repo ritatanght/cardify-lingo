b5eb032ec6d583621ed72b7fb62b187633be2cc9
ritatanght <doggymint@gmail.com>
2023-12-26 22:16:40 -0500

    Remove unused classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className="columns-container text-center flex flex-col gap-6 md:gap-4 md:flex-row justify-between w-11/12 mt-6 mb-4 mx-auto">
-        <div className="column basis-2/6 rounded-md p-4 bg-color-1/60">
+      <div className="text-center flex flex-col gap-6 md:gap-4 md:flex-row justify-between w-11/12 mt-6 mb-4 mx-auto">
+        <div className="basis-2/6 rounded-md p-4 bg-color-1/60">
           <TbCards
             aria-hidden="true"
             className="text-6xl drop-shadow inline text-gray-50"
@@ -42,7 +42,7 @@ export default function Home() {
             visual aids.
           </p>
         </div>
-        <div className="column basis-2/6 rounded-md p-4 bg-color-4/70">
+        <div className="basis-2/6 rounded-md p-4 bg-color-4/70">
           <RiSpeakLine
             aria-hidden="true"
             className="text-6xl drop-shadow inline text-gray-50"
@@ -56,7 +56,7 @@ export default function Home() {
             learning experience.
           </p>
         </div>
-        <div className="column basis-2/6 rounded-md p-4 bg-color-2/80">
+        <div className="basis-2/6 rounded-md p-4 bg-color-2/80">
           <RiCommunityLine
             aria-hidden="true"
             className="text-6xl drop-shadow inline text-gray-50"
