72ec265fd8443e142418219feafde380b15c5743
ritatanght <doggymint@gmail.com>
2023-12-29 18:09:52 -0500

    Extract the visit to login page to perform beforeEach

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,14 @@
 describe("Login", () => {
-  it("should prompt errors when email and password are both blank", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000/login");
+  });
 
+  it("should prompt errors when email and password are both blank", () => {
     cy.get("button").contains("Submit").click();
     cy.get(".Toastify__toast-body").contains("Fields cannot be empty");
   });
 
   it("should prompt errors to users for incorrect email", () => {
-    cy.visit("http://localhost:3000/login");
     cy.get("input[type=email]").type("john.doe@example.com");
     cy.get("input[type=password]").type("123456{enter}");
 
@@ -15,7 +16,6 @@ describe("Login", () => {
   });
 
   it("should prompt errors to users for incorrect password", () => {
-    cy.visit("http://localhost:3000/login");
     cy.get("input[type=email]").type("john@example.com");
     cy.get("input[type=password]").type("johnssecurepassword{enter}");
 
@@ -23,7 +23,6 @@ describe("Login", () => {
   });
 
   it("should redirect to profile page after successful login with email and password", () => {
-    cy.visit("http://localhost:3000/login");
     cy.get("input[type=email]").type("john.doe@example.com");
     cy.get("input[type=password]").type("johnssecurepassword{enter}");
     cy.get(".Toastify__toast-body").contains("Login successful");
@@ -32,5 +31,4 @@ describe("Login", () => {
     // The new page should contain an h1 with the user's "username"
     cy.get("h1").contains("john_doe");
   });
-
 });
