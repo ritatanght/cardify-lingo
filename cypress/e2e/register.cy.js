1960c498de3241b8ad4428fa349f6fa8f69d62cf
ritatanght <doggymint@gmail.com>
2023-12-29 22:13:01 -0500

    Add tests for registering with valid info and duplicate email address

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,9 +1,16 @@
 describe("Register", () => {
-  it("should prompt error when password and confirm password fields do not match", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000/register");
 
     cy.get('input[aria-label="email"]').type("user@example.com");
     cy.get('input[aria-label="username"]').type("user");
+  });
+
+  // before(()=>{
+  //   cy.exec("npm run db:reset");
+  // })
+
+  it("should prompt error when password and confirm password fields do not match", () => {
     cy.get('input[aria-label="Password"]').type("123456");
     cy.get('input[aria-label="Confirm Password"]').type("543210");
     cy.get("button").contains("Submit").click();
@@ -12,16 +19,39 @@ describe("Register", () => {
   });
 
   it("should prompt error when password has length less than 6", () => {
-    cy.visit("http://localhost:3000/register");
-
-    cy.get('input[aria-label="email"]').type("user@example.com");
-    cy.get('input[aria-label="username"]').type("user");
     cy.get('input[aria-label="Password"]').type("12345");
     cy.get('input[aria-label="Confirm Password"]').type("12345");
     cy.get("button").contains("Submit").click();
 
     cy.get(".Toastify__toast-body").contains(
       "Password must be at least 6 in length"
+    );
+  });
+
+  it("should redirect to profile page upon successful registration", () => {
+    cy.get('input[aria-label="Password"]').type("123456");
+    cy.get('input[aria-label="Confirm Password"]').type("123456");
+    cy.get("button").contains("Submit").click();
+
+    // Redirect to the profile page
+    cy.url().should("include", "/profile");
+    cy.get("h1").contains("user");
+
+    // brand new user with no created/ favorited set
+    const profileMain = cy.get("main");
+    profileMain.find("button").contains("My Sets");
+    profileMain.find("p").contains("You don't have any sets yet.");
+    profileMain.find("button").contains("Favorite Sets").click();
+    profileMain.find("p").contains("You have not favorited any sets yet.");
+  });
+
+  it("should prompt error for an existing account with the same email", () => {
+    cy.get('input[aria-label="Password"]').type("123456");
+    cy.get('input[aria-label="Confirm Password"]').type("123456");
+    cy.get("button").contains("Submit").click();
+
+    cy.get(".Toastify__toast-body").contains(
+      "An account with this email address already exists"
     );
   });
 });
