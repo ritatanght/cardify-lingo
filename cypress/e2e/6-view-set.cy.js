7b03c7ace88de609e79ad7fbe4f033361a6bab38
ritatanght <doggymint@gmail.com>
2023-12-30 21:57:48 -0500

    Add test for loading the view set page

---
+++ cypress/e2e/6-view-set.cy.js
@@ -0,0 +1,22 @@
+describe("View", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/sets/17");
+  });
+  it("should only see the front side of the first card on visit of the view set page", () => {
+    cy.fixture("testSet").then((testSet) => {
+      cy.get("h1").contains(testSet.title);
+
+      const cards = cy.get(".Card");
+      cards.should("have.length", testSet.cards.length);
+      cards.each(($card, index) => {
+        cy.wrap($card)
+          .contains(testSet.cards[index].front)
+          .should(index === 0 ? "be.visible" : "not.be.visible");
+        cy.wrap($card)
+          .contains(testSet.cards[index].back)
+          .should("not.be.visible");
+      });
+    });
+  });
+
+});
